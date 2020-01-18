c8824dcc2d2440a888d293d2096a9545f5ae1536
Tommy Wu <zhiwenw@vmware.com>
2020-01-19 00:58:01 +0800

    Solution2, bugs still
    should use states tree

---
+++ grid.py
@@ -0,0 +1,116 @@
+import os
+import json
+import gc
+from pprint import pprint
+
+UP, DOWN, LEFT, RIGHT = "U", "D", "L", "R"
+
+EMPTY, MINNOR, OBSTRUCTIONS, SUCCEED, WALL = 0, 1, 2, 3, 4
+SLASH, BACK_SLASH = "/", "\\"
+
+class InvalidSetGridError(Exception):
+    pass
+
+class Grid(object):
+
+    def __init__(self, row, col, minors, obstructions):
+        self.row = row
+        self.col = col
+        self.grid = [[ EMPTY for i in range(self.row)] for j in range(self.col)]
+        self.minors = [(x, y) for y,x in minors]
+        self.obstructions = [(x, y) for y,x in obstructions]
+        for x,y in self.minors:
+            self.set_object(x, y, MINNOR)
+        for x,y in self.obstructions:
+            self.set_object(x, y, OBSTRUCTIONS)
+
+    def set_object(self, x, y, t):
+        """
+        t: type
+        """
+        if y >= self.row or x >= self.col or x <0 or y <0:
+            raise InvalidSetGridError(f"x: {x}, y: {y}, col: {self.col} row: {self.row}, ")
+        self.grid[x][y] = t
+
+    def get_object(self, x, y):
+        if x == self.col and y == 0:
+            return SUCCEED
+        if y >= self.row or x >= self.col or x <0 or y <0:
+            return WALL
+        return self.grid[x][y]
+
+    def __str__(self):
+        readable_data = []
+        for y in range(self.row):
+            row = []
+            for x in range(self.col):
+                row.append(self.grid[x][y])
+            readable_data.append(row)
+        pprint(readable_data)
+        return ""
+
+
+def get_grid_from_data(data):
+    row = data['rows']
+    col = data['cols']
+    obstructions = data['obstructions']
+    mirrors = data['mirrors']
+    del data
+    return Grid(row, col, mirrors, obstructions)
+
+def peek_maps():
+    dir_path = 'maps'
+    fn = os.listdir(dir_path)
+    datas = []
+    for f in fn:
+        data = get_single_data_from_file(f)
+        if not data:
+            continue
+        data['name'] = f
+        datas.append(data)
+    for data in sorted(datas, key=lambda v: v["difficulty"]):
+        print(f"{data['name']} - {data['difficulty']} - row: {data['rows']}, col: {data['cols']}, mirror: {len(data['mirrors'])}")
+
+def get_solutions(solution_func):
+    dir_path = 'maps'
+    fs = os.listdir(dir_path)
+    output = {}
+    output['solutions'] = []
+    output['name'] = 'tommy'
+    skip_list = ['m29.json']
+    # skip_list = []
+    for filename in fs:
+        gc.collect()
+        print("####################")
+        print("Processing ", filename)
+        if filename in skip_list:
+            continue
+        with open(os.path.join(dir_path, filename), 'r') as f:
+            try:
+                data = json.load(f)
+            except json.decoder.JSONDecodeError as e:
+                print("Invalid json for:", filename)
+                continue
+        print(f"row: {data['rows']}, col {data['cols']}, M: {len(data['mirrors'])}")
+        result = None
+        try:
+            result = solution_func(data)
+            del data
+        except Exception as e:
+            print(e)
+        if result:
+            print(f"Shoutpath dist: {result[1]}")
+        else:
+            print("Invalid")
+
+
+
+def get_single_data_from_file(filename):
+    dir_path = 'maps'
+    with open(os.path.join(dir_path, filename), 'r') as f:
+        try:
+            data = json.load(f)
+        except json.decoder.JSONDecodeError as e:
+            print("Error decode:", filename)
+            return None
+    return data
---
+++ solution2.py
@@ -0,0 +1,330 @@
+
+from unittest.mock import Mock
+from pprint import pprint
+from grid import *
+from heapq import heappush, heappop
+
+import sys
+import resource
+soft, hard = 10 * 10**9, 10 * 10**9
+# soft, hard = 10**8, 10**8   # uncommenting this allows program to finish
+resource.setrlimit(resource.RLIMIT_AS,(soft, hard))
+
+class Solution:
+
+    MIRROR_DIRECTION_MAP = {
+        UP: [RIGHT, LEFT],
+        DOWN: [LEFT, RIGHT],
+        LEFT: [UP, DOWN],
+        RIGHT: [UP, DOWN]
+    }
+
+    def get_minnor_position(self, p1, p2, p3):
+        assert (p1['x'] == p2['x']) ^ (p1['y'] == p2['y'])
+        assert (p2['x'] == p3['x']) ^ (p2['y'] == p3['y'])
+        if p2['x'] > p1['x']: #right
+            if p3['y'] > p2['y']:
+                return BACK_SLASH
+            if p3['y'] < p2['y']:
+                return SLASH
+        if p2['x'] < p1['x']: # left
+            if p3['y'] > p2['y']:
+                return SLASH
+            if p3['y'] < p2['y']:
+                return BACK_SLASH
+        if p2['y'] < p1['y']: # up
+            if p3['x'] < p2['x']:
+                return BACK_SLASH
+            if p3['x'] > p2['x']:
+                return SLASH
+        if p2['y'] > p1['y']: # down
+            if p3['x'] < p2['x']:
+                return SLASH
+            if p3['x'] > p2['x']:
+                return BACK_SLASH
+        raise Exception(f"{p1}\n{p2}\n{p3}")
+
+    def solve(self, grid):
+        self.grid = grid
+        self.graph = Graph(len(self.grid.minors) + 2)
+        self.generate_graph()
+        end_p = (self.grid.col, 0)
+        if  end_p not in self.nodes_by_x_y:
+            return None
+        start = self.nodes_by_x_y[(-1, 0)]
+        path, min_dist = self.graph.shorest_path(start, self.nodes_by_x_y[end_p])
+
+        grid_path = []
+        last_node = None
+        for i, p in enumerate(path):
+            node = self.nodes[p.w]
+            if i != 0:
+                print(f"{path[i-1].w, p.w, p.v}")
+                node['minnor'] = self.get_minnor_position(last_node, node, self.nodes[p.v])
+            grid_path.append(node)
+            if i == len(path) - 1:
+                grid_path.append(self.nodes[p.v])
+            last_node = node
+
+        return grid_path, min_dist
+
+    def generate_graph(self):
+        point = {
+            'x': -1,
+            'y': 0,
+            'd': RIGHT,
+            'type': EMPTY
+        }
+        self.visited_path = set()
+        self.mirror_stack = []
+        self.mirror_stack.append(point)
+        self.visited_mirror_by_xy = {}
+        self.visited_mirror_by_xy[(point['x'],point['y'])] = point
+        self.nodes = []
+        self.nodes_by_x_y = {}
+        for i, m in enumerate(self.grid.minors):
+            self.nodes.append(
+                {'x': m[0], 'y': m[1]}
+            )
+            self.nodes_by_x_y[m] = i
+        self.nodes.append(point)
+        self.nodes_by_x_y[(point['x'], point['y'] )] = len(self.nodes) - 1
+        self.nodes.append({'x': self.grid.col, 'y': 0})
+        self.nodes_by_x_y[(self.grid.col, 0)] = len(self.nodes) - 1
+        self.stack = []
+        self.stack.append(point)
+        while self.stack:
+            point = self.stack.pop()
+            if point['type'] == MINNOR or point['type'] == SUCCEED:
+                if (point["x"], point["y"]) not in self.nodes_by_x_y:
+                    self.nodes.append(point)
+                if self.mirror_stack:
+                    self.check_mirro_stack()
+                    last_mirror = self.mirror_stack[-1]
+                    try:
+                        self.check_mirro_stack()
+                        edge = self.cal_edge(self.mirror_stack[-2], last_mirror, point)
+                        self.graph.add_edge(edge)
+                        try:
+                            self.check_graph(point)
+                        except Exception as e:
+                            pass
+                    except IndexError:
+                        edge = self.cal_edge(None, last_mirror, point)
+                        self.graph.add_edge(edge)
+                self.mirror_stack.append(point)
+                self.check_mirro_stack()
+                self.visited_mirror_by_xy[(point['x'], point['y'])] = True
+            next_points = self.get_next_points(point['x'], point['y'], point['d'])
+            if not next_points:
+                self.backward()
+
+
+            for p in next_points:
+                self.stack.append(p)
+
+
+    def check_graph(self, p):
+        start = self.nodes_by_x_y[(-1, 0)]
+        c = self.nodes_by_x_y[(p['x'], p['y'])]
+        path, min_dist = self.graph.shorest_path(start, c)
+
+        grid_path = []
+        last_node = None
+        for i, p in enumerate(path):
+            node = self.nodes[p.w]
+            if i != 0:
+                print(f"{path[i-1].w, p.w, p.v}")
+                node['minnor'] = self.get_minnor_position(last_node, node, self.nodes[p.v])
+            grid_path.append(node)
+            if i == len(path) - 1:
+                grid_path.append(self.nodes[p.v])
+            last_node = node
+
+    def check_mirro_stack(self):
+        if len(self.mirror_stack) >=3:
+            for i in range(len(self.mirror_stack) - 2):
+                p1 = self.mirror_stack[i]
+                p2 = self.mirror_stack[i+1]
+                p3 = self.mirror_stack[i+2]
+                if ((p1['x'] == p2['x'] == p3['x']) or (p1['y'] == p2['y'] == p3['y'])):
+                    raise Exception("Check error")
+
+    def backward(self):
+        last_m = self.mirror_stack.pop()
+        self.visited_mirror_by_xy.pop((last_m['x'], last_m['y']))
+        if self.stack:
+            back_point = self.stack[-1]
+            if back_point['x'] != last_m['x'] or back_point["y"] != last_m["y"]:
+                while True:
+                    last_m = self.mirror_stack.pop()
+                    self.visited_mirror_by_xy.pop((last_m['x'], last_m['y']))
+                    if last_m['x'] == back_point['x'] and last_m['y'] == back_point['y']:
+                        break
+
+    def cal_edge(self, p0, p1, p2):
+        assert (p1['x'] == p2['x']) ^ (p1['y'] == p2['y']), f"p1 {p1}, p2 {p2}"
+        if p0:
+            assert (p0['x'] == p1['x']) ^ (p0['y'] == p1['y']), f"p1 {p0}, p2 {p1}"
+            if (p0['x'] == p1['x'] == p2['x']) or (p0['y'] == p1['y'] == p2['y']):
+                raise Exception(f"Error: {p0} {p1} {p2}")
+        if p1['x'] == p2['x']:
+            wt = abs(p2['y'] - p1['y'])
+        else:
+            wt = abs(p2['x'] - p1['x'])
+        return Edge(self.nodes_by_x_y[(p1['x'], p1['y'])], self.nodes_by_x_y[(p2['x'], p2['y'])], wt)
+
+    def get_next_points(self, x, y, direction):
+        while True:
+            if direction == UP:
+                y -= 1
+            if direction == DOWN:
+                y += 1
+            if direction == LEFT:
+                x -= 1
+            if direction == RIGHT:
+                x += 1
+            state = self.grid.get_object(x, y)
+            if state == MINNOR:
+                if (x, y) in self.visited_mirror_by_xy:
+                    return []
+                if (x, y, direction) not in self.visited_path:
+                    self.visited_path.add((x, y, direction))
+                    directs = self.MIRROR_DIRECTION_MAP[direction]
+                    p1 = (x, y, directs[0])
+                    p2 = (x, y, directs[1])
+                    res = []
+                    res.append({'x':x , 'y': y, 'd': p1[2], 'type': MINNOR})
+                    res.append({'x':x , 'y': y, 'd': p2[2], 'type': MINNOR})
+                    return res
+                else:
+                    return []
+            if state == SUCCEED:
+                return [{'x':x , 'y': y, 'd': direction, 'type': SUCCEED}]
+
+            if state == WALL:
+                return []
+            if state == OBSTRUCTIONS:
+                return []
+
+
+class Edge:
+
+    def __init__(self, w, v, wt):
+        self.w = w
+        self.v = v
+        self.wt = wt
+
+    def __hash__(self):
+        return self.w * 20 +  self.v + 3  + self.wt *14
+
+    def __eq__(self, other):
+        return self.w == other.w and self.v == other.v and self.wt == other.wt
+
+    def __str__(self):
+        return f"({self.w}->{self.v}: {self.wt})"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __le__(self, other):
+        return self.wt <= other.wt
+
+    def __lt__(self, other):
+        return self.wt < other.wt
+
+    def __gt__(self, other):
+        return self.wt > other.wt
+
+    def __ge__(self, other):
+        return self.wt >= other.wt
+
+
+class Graph:
+
+    def __init__(self, v):
+        self.v = v
+        self.g = [set() for i in range(v)]
+
+    def add_edge(self, edge):
+
+        self.g[edge.w].add(edge)
+
+    def __str__(self):
+        pprint(self.g)
+        return ""
+
+    def __repr__(self):
+        return self.__str__()
+
+    def shorest_path(self, start, dest):
+        heap = []
+        dist_to = [0 for i in range(self.v)]
+        from_edge = [-1 for i in range(self.v)]
+        visited = [False for i in range(self.v)]
+        s_edge = Edge(start, start, 0)
+        s_path = Path(start, start)
+        dist_to[start] = 0
+        from_edge[start] = s_edge
+        heappush(heap, s_path)
+        assert self.g[start], 'at least one edge'
+        while heap:
+            p = heappop(heap)
+            if (not dist_to[p.to]) or (p.wt < dist_to[p.to]):
+                dist_to[p.to] = p.wt
+                visited[p.to] = True
+                if p.to == dest:
+                    break
+            for adj in self.g[p.to]:
+                if not visited[adj.v]:
+                    from_edge[adj.v] = adj
+                    heappush(heap, Path(adj.v, dist_to[adj.w] + adj.wt))
+
+        stack =[]
+        edge = from_edge[dest]
+        if(isinstance(edge, int)):
+            raise Exception("No shortest path")
+        while edge.v != edge.w:
+            stack.append(edge)
+            edge = from_edge[edge.w]
+        stack.reverse()
+        return stack, dist_to[dest]
+
+class Path:
+    def __init__(self, to, wt):
+        self.to = to
+        self.wt = wt
+
+    def __le__(self, other):
+        return self.wt <= other.wt
+
+    def __lt__(self, other):
+        return self.wt < other.wt
+
+    def __gt__(self, other):
+        return self.wt > other.wt
+
+    def __ge__(self, other):
+        return self.wt >= other.wt
+
+
+
+
+def solve(data):
+    grid = get_grid_from_data(data)
+    sol = Solution()
+    try:
+        res = sol.solve(grid)
+    except InvalidSetGridError as e:
+        print(e)
+        print("Invalid set grid")
+    return res
+
+if __name__ == '__main__':
+    # peek_maps()
+    fn = "m12.json"
+    data = get_single_data_from_file(fn)
+    print(solve(data))
+
+
+    # get_solutions(solve)
